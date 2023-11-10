904f0d46775a953ce374607f17eaaa1e20cfa38f
MKuzera <mateusz.qzera@gmail.com>
2023-11-10 12:01:27 +0100

    To do list added

--- main.py
+++ main.py
@@ -4,6 +4,15 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+#cel ->
+# wprowdzic dane
+# zrobic te 2 funkcje
+# wprowadzci ta funkcje testowa  isprawdzic na niej algorytmy
+# przygotowac funkcje problemu rzeczywistego..
+# wykresy i inne
+# sprawko
+# noi fajrancik
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
