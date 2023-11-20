388ab53f308741975377520424adf1db2c307d58
MKuzera <mateusz.qzera@gmail.com>
2023-11-20 17:12:07 +0100

    -deleted trash

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ if __name__ == '__main__':
     beta = 0.1
 
 
-
     randoms = np.array([np.random.uniform(-1, 1, 2) for _ in range(N)])
 
     with open('rb.csv', mode='w', newline='') as file:
@@ -54,29 +53,7 @@ if __name__ == '__main__':
                 x,y,counter =hj.optimize()
                 writer.writerow([s, x0[0], x0[1], x[0], x[1], y, counter])
 
-    # with open('hj.csv', mode='w', newline='') as file:
-    #     writer = csv.writer(file)
-    #   #  writer.writerow(['s', 'x1(0)', 'x2(0)', 'x1*', 'x2*', 'y*', 'function_calls'])
-    #     for s in s0_values:
-    #         for i in range(N):
-    #             x0 = randoms[i]
-    #             x = hooke_jeeves(x0,s,epsilon,objective_function)
-    #
-    #
-    #           #  hj = HJOptimizer(x0, np.array([s, s]), alfaHJ, epsilon, Nmax, funkcjaCelu)
-    #          #  x,y,counter =hj.optimize()
-    #
-    #             x1 = objective_function(x)[0]
-    #             print("p")
-    #             print(x1[0])
-    #             print(x1[1])
-    #             print(funkcjaCelu([1 ,1]))
-    #             print(funkcjaCelu([x1[0],x1[1]]))
-    #             print(objective_function([x1[0], x1[1]]))
-    #
-    #             writer.writerow([s, x0[0], x0[1], x[0], funkcjaCelu([x1[0],x1[1]])])
-    #
-    #
+
 
     x = np.array([-0.5,1.0])
     s0 = np.array([0.5, 0.5])
